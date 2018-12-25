284e4013a1ac877d1ba6538eecd52c4ecbdcc367
KawasakiDaiki <kawadaiki1300@gmail.com>
2018-12-26 00:31:27 +0900

    制作ツール制作中

--- SAGA/Assets/Script/Confi/NotesCle.cs
+++ SAGA/Assets/Script/Confi/NotesCle.cs
@@ -1,45 +1,60 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class NotesCle : MonoBehaviour {
     public GameObject note;
+    CSVWrite CSVW;
     [SerializeField] GameObject DefReen;
     Vector3 Vec;
-	// Use this for initialization
-	void Start () {
+    float delta, _startTime;
+    // Use this for initialization
+    void Start () {
         Vec = new Vector3(DefReen.transform.position.x, 0, 0);
-	}
+        CSVW = GetComponent<CSVWrite>();
+        _startTime = Time.time;
+    }
 	
 	// Update is called once per frame
 	void Update () {
+        BGet();
+    }
+    void BGet()
+    {
         if (Input.GetKeyDown(KeyCode.A))
         {
-            Instantiate(note, new Vector3(DefReen.transform.position.x + 0.5f * 0, 0, 0), Quaternion.identity);
+            WriteC(0);
         }
         if (Input.GetKeyDown(KeyCode.S))
         {
-            Instantiate(note, new Vector3(DefReen.transform.position.x + 0.5f * 1, 0, 0), Quaternion.identity);
+            WriteC(1);
         }
         if (Input.GetKeyDown(KeyCode.D))
         {
-            Instantiate(note, new Vector3(DefReen.transform.position.x + 0.5f * 2, 0, 0), Quaternion.identity);
+            WriteC(2);
         }
         if (Input.GetKeyDown(KeyCode.F))
         {
-            Instantiate(note, new Vector3(DefReen.transform.position.x + 0.5f * 3, 0, 0), Quaternion.identity);
+            WriteC(3);
         }
         if (Input.GetKeyDown(KeyCode.J))
         {
-            Instantiate(note, new Vector3(DefReen.transform.position.x + 0.5f * 4, 0, 0), Quaternion.identity);
+            WriteC(4);
         }
         if (Input.GetKeyDown(KeyCode.K))
         {
-            Instantiate(note, new Vector3(DefReen.transform.position.x + 0.5f * 5, 0, 0), Quaternion.identity);
+            WriteC(5);
         }
         if (Input.GetKeyDown(KeyCode.L))
         {
-            Instantiate(note, new Vector3(DefReen.transform.position.x + 0.5f * 6, 0, 0), Quaternion.identity);
+            WriteC(6);
         }
+    }
+    void WriteC(float num)
+    {
+        CSVW.WriteCSV(GetTiming().ToString() + "," + num.ToString());
+        Instantiate(note, new Vector3(DefReen.transform.position.x + 0.5f * num, 0, 0), Quaternion.identity);
+    }
+    float GetTiming()
+    {
+        return Time.time - _startTime;
     }
 }
